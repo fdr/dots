f4640d2ea3b27fba041a131d596b7d5e8ddf7ec4
Jason Dusek <jason.dusek@gmail.com>
2009-08-25 23:57:42 -0700

    Now offers specific installation instructions for sup.

--- install/link-source.sh
+++ install/link-source.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-for f in source/*
+tops='    source/bashrc       source/ghci         source/gvimrc
+          source/haskeline    source/irbrc        source/irssi
+          source/screenrc     source/vim          source/vimrc
+'
+
+for f in $tops 
 do
   here="${PWD}/${f}"
   there="${HOME}/.`basename ${f}`"
@@ -10,4 +15,16 @@ do
   eval ${foo}
 done
 
+sup="${HOME}/.sup"
+if [ -d "$sup" ]
+then
+  rm "$sup/config.yaml"
+  rm -rf "$sup/hooks"
+  for f in source/sup/config.yaml source/sup/hooks
+  do
+    here="$PWD/$f"
+    there="$sup/.`basename $f`"
+    ln -s $here $there
+  done
+fi
 
