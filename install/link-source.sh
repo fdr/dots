6545632b1cc89bd6654bc71535aaccc682ddeed3
Jason Dusek <jason.dusek@gmail.com>
2009-08-26 00:09:00 -0700

    So annoying, the way this program acts.

--- install/link-source.sh
+++ install/link-source.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-tops='    source/bashrc       source/ghci         source/gvimrc
-          source/haskeline    source/irbrc        source/irssi
-          source/screenrc     source/vim          source/vimrc
-'
-
-for f in $tops 
+for f in source/*
 do
   here="${PWD}/${f}"
   there="${HOME}/.`basename ${f}`"
@@ -14,17 +9,4 @@ do
   eval ${clean}
   eval ${foo}
 done
-
-sup="${HOME}/.sup"
-if [ -d "$sup" ]
-then
-  rm "$sup/config.yaml"
-  rm -rf "$sup/hooks"
-  for f in source/sup/config.yaml source/sup/hooks
-  do
-    here="$PWD/$f"
-    there="$sup/.`basename $f`"
-    ln -s $here $there
-  done
-fi
 
